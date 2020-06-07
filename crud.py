e9d42e78e50e5ea7dbd05d3ccb359f9af16b09e2
Shira Amrany <shira.amrany@gmail.com>
2020-06-07 20:15:48 +0000

    files added

---
+++ crud.py
@@ -0,0 +1,203 @@
+"""CRUD operations."""
+
+from model import db, User, Recipe, Fave_recipes, Ingredient, Recipe_ingredients, User_no_goes, connect_to_db
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+#connects to the database ^
+
+def create_user(email, password, first_name, last_name):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password, first_name = first_name, last_name = last_name)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+def add_new_ingredient(ingredient_name,dairy_free = True, gluten_free = True,
+                        vegetarian = True, vegan = True, paleo = True):
+    """Add new ingredient to db"""
+    current_ingredient = Ingredient(ingredient_name = ingredient_name, 
+                        dairy_free = dairy_free, gluten_free = gluten_free,
+                        vegetarian = vegetarian, vegan = vegan, paleo = paleo)
+
+    # float the metric amount
+
+    db.session.add(current_ingredient)
+    db.session.commit()
+
+    return current_ingredient
+
+def all_ingredients():
+    """Return all ingredients."""
+
+    return Ingredient.query.all()
+
+def get_ingredient_id(ingredient_name):
+    """Returns an ingredient id given the ingredient name"""
+
+    current_ingredient = Ingredient.query.filter_by(ingredient_name = ingredient_name).one()
+    current_id = current_ingredient.ingredient_id
+    
+    return current_id
+
+
+def get_ingredient_name(ingredient_id):
+    """Returns an ingredient name given the ingredient id"""
+
+    current_ingredient = Ingredient.query.get(ingredient_id)
+    current_name = current_ingredient.ingredient_name
+    
+    return current_name
+
+def return_all_ingredients_by_name():
+    """Return all ingredients by name in a list"""
+
+    existing_ingredients = []
+    ingredients = all_ingredients() # a list of objects
+    for ingredient in ingredients:
+        name = ingredient.ingredient_name
+        existing_ingredients.append(name)
+
+    return existing_ingredients
+
+
+def add_needed_ingredient(recipe_id, ingredient_id, amount, metric = None, preparation = None):
+    """Add ingredient in recipe to db"""
+    current_needed_ingredient = Recipe_ingredients(recipe_id = recipe_id, ingredient_id = ingredient_id, 
+                                                   amount = amount, metric = metric, preparation = preparation)
+    # CAN CHANGE ID TO RECIPE AND INGREDIENT OBJECTS
+    db.session.add(current_needed_ingredient)
+    db.session.commit()
+
+    return current_needed_ingredient
+
+# def  add_list_of_recipe_ingredients(recipe_id, ingredients):
+# #     """Takes a dictionary of ingredients, related recipe id, and instantiates objects to commit to the db"""
+# #     for ingredient in ingredients:
+# #     name = ingredient.get('ingredient_name')
+# #     if name not in existing_ingredients:
+# #         add_new_ingredient(name)
+# #       ##### Doesn't account for any attributes of the ingredient nor ensure case consistency
+    
+#     for ingredient in ingredients:
+#         name = ingredient.get('ingredient_name')
+
+#         current_ingredient = Ingredient.query.filter_by(ingredient_name = ingredient_name).one()
+#         if name not in existing_ingredients:
+#             add_new_ingredient(name)
+#           ##### Doesn't account for any attributes of the ingredient nor ensure case consistency
+        
+#         ingredient_id = get_ingredient_id(name)
+#         print(ingredient_id)
+#         amount = ingredient["amount"]
+#         print(amount)
+#         metric = ingredient["metric"]
+#         print(metric)
+#         preparation = ingredient["preparation"]
+#         print(preparation)
+
+#         add_needed_ingredient(recipe_id, ingredient_id, amount, metric, preparation)
+          
+
+
+    # current_ingredient_in_recipe = 4
+
+    # for ingredient in ingredients:
+    #     name = ingredient.get('ingredient_name')
+    #     print(name)
+    #     ingredient_id = get_ingredient_id(name)
+    #     print(ingredient_id)
+    #     amount = ingredient.get('amount')
+    #     print(amount)
+    #     metric = ingredient.get('metric')
+    #     print(metric)
+    #     prepartion = ingredient.get('preparation')
+    #     print(preparation)
+
+    #     # current_object = Recipe_ingredients(recipe_id = recipe_id, ingredient_id = ingredient_id,
+    #     #                                     amount = amount, metric = metric, preparation = preparation)
+    #     # db.session.add(current_object)
+    #     # db.session.commit()
+
+    # return current_ingredient_in_recipe
+    # # what am I actually returning here? ingredient objects that relate to this recipe?
+
+
+def create_new_recipe(recipe_name, source_name, website, directions, image, servings, ingredients): 
+    """Create and return new recipes, add ingredients to database if not there, and add ingredients 
+    in recipes where ingredients is a list of dictionary objects."""
+
+    # Add recipe to db
+    current_recipe = Recipe(recipe_name = recipe_name, source_name = source_name,
+        website = website, directions = directions, image = image, servings = servings)
+    db.session.add(current_recipe)
+    db.session.commit()
+
+    # get a list of existing ingredients in db and commit ingredients from recipe to db if not already in.
+    existing_ingredients = return_all_ingredients_by_name() # REVISIT
+    current_ingredient_objects = all_ingredients()
+    current_recipe_id = current_recipe.recipe_id
+
+    for ingredient in ingredients: #ingredient is a dictionary
+        name = ingredient.get('ingredient_name') #access key-value pair
+        if name not in existing_ingredients:
+            ingredient_id = add_new_ingredient(name).ingredient_id
+
+        else:
+            ingredient_obj = Ingredient.query.filter_by(ingredient_name = name).first() # QUERYING EVERYTIME
+            ingredient_id = ingredient_obj.ingredient_id 
+          ##### Doesn't account for any attributes of the ingredient nor ensure case consistency
+        amount = ingredient.get('amount', None)
+        metric = ingredient.get('metric', None) 
+        preparation = ingredient.get('preparation', None)
+        add_needed_ingredient(current_recipe_id, ingredient_id, amount, metric, preparation)
+
+
+    return current_recipe
+
+
+def all_users():
+    """Returns all users."""
+
+    return User.query.all()
+
+def all_recipes():
+    """Returns all recipes."""
+
+    return Recipe.query.all()
+
+
+def get_user_by_id(user_id):
+    """Returns user object given a user_id"""
+    user = User.query.get(user_id)
+    return user
+
+def get_recipe_by_id(recipe_id):
+    """Returns a recipe object given a recipe_id"""
+    recipe = Recipe.query.get(recipe_id)
+    
+    return recipe
+
+
+def fave_recipe(user, recipe):
+    """Save a favorite recipe to the db"""
+    fave_recipe = Fave_recipes(user_id = user.user_id, recipe_id = recipe.recipe_id)
+
+    db.session.add(fave_recipe)
+    db.session.commit()
+
+    return fave_recipe
+
+def no_go(user, ingredient):
+    """Save a favorite recipe to the db"""
+    no_go = User_no_goes(user_id = user.user_id, ingredient_id = ingredient.ingredient_id)
+
+    db.session.add(no_go)
+    db.session.commit()
+
+    return no_go
---
+++ seed_database.py
@@ -0,0 +1,61 @@
+""" Script to seed datatase."""
+import os
+import json
+from random import choice, randint  
+import crud
+import model
+import server
+
+
+
+os.system('dropdb recipes')
+os.system('createdb recipes')
+
+# Enable loading data from JSON file
+model.connect_to_db(server.app) 
+model.db.create_all()
+
+
+# Load recipe info from JSON file
+with open('data/recipes.json') as f:
+    recipes = json.loads(f.read())
+
+recipes_in_db = []
+
+
+for recipe in recipes:
+
+	recipe_name, source_name, website, directions, image, servings, ingredients = (
+		recipe['recipe_name'],
+		recipe['source_name'],
+		recipe['website'],
+		recipe['directions'],
+		recipe['image'],
+		recipe['servings'],
+		recipe['ingredients'])
+
+
+	db_recipe = crud.create_new_recipe(recipe_name, source_name, website, 
+									   directions, image, servings, ingredients)
+	recipes_in_db.append(db_recipe)
+
+
+# Create Test Users
+users_in_db = []
+ingredients_in_db = crud.all_ingredients()
+for n in range(1,10):
+	email = f'user{n}@test.com'
+	password = 'test'
+	first_name = f'first{n}'
+	last_name = f'last{n}'
+	user = crud.create_user(email, password, first_name, last_name)
+	users_in_db.append(user)
+	
+	#create user_faves
+	for i in range(5):
+		random_recipe = choice(recipes_in_db)
+		random_user = choice(users_in_db)
+		random_ingredient = choice(ingredients_in_db)
+		crud.fave_recipe(random_user, random_recipe)
+		crud.no_go(random_user, random_ingredient)
+
